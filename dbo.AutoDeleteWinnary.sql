ed81f2cc0064e2fa70305fc9d97cf575c278e5d3
GMIRIC <gmiric98@gmail.com>
2021-06-02 23:07:37 +0200

    Triger

---
+++ dbo.AutoDeleteWinnary.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER AutoDeleteWinnary
+ON WinnarySet
+INSTEAD OF DELETE
+AS
+DECLARE 
+	@targetId int
+BEGIN
+	SELECT @targetId = i.Id FROM deleted i;
+	DELETE FROM EmployeesSet1 WHERE EmployeesSet1.WinnaryId=@targetId;
+	DELETE FROM WinnarySet WHERE WinnarySet.Id=@targetId;
+END
